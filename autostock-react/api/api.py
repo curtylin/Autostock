cbc78d7c9462c95a1394d8254863ec49dea1a6bb
rftf <u1025269@utah.edu>
2021-11-19 02:26:20 -0700

    Added route for static router file as part of an error handler for gunicorn

--- autostock-react/api/api.py
+++ autostock-react/api/api.py
@@ -2,6 +2,10 @@ from flask import Flask
 
 app = Flask(__name__, static_folder="../build", static_url_path="/")
 
+@app.errorhandler(404)
+def not_found(error):
+    return app.send_static_file('index.html')
+
 @app.route('/')
 def index():
     return app.send_static_file('index.html')
