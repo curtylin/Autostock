47ce399ec7e09cd7f861ff19bed42f1aa8a1060c
Nick <nickmountz16@gmail.com>
2021-11-22 12:57:43 -0700

    cleanup/basic page linking

--- auto-stock/AutoStock-React/src/pages/index.tsx
+++ auto-stock/AutoStock-React/src/pages/index.tsx
@@ -1,20 +1,13 @@
 import * as React from "react"
-import { StaticImage } from "gatsby-plugin-image"
+import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <Seo title="Home" />
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
+    <Seo title="AutoStock" />
+    <Link to='/CreateAlgorithm'>Create New Algorithm</Link>
   </Layout>
 )
 
