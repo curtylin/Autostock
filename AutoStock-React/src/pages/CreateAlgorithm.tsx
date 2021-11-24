c03fa419e5e0f5703e846cdd017639a9ca563af5
BrandonNham <u1059350@utah.edu>
2021-11-23 20:56:48 -0700

    Added to Create Algorithm

--- AutoStock-React/src/pages/CreateAlgorithm.tsx
+++ AutoStock-React/src/pages/CreateAlgorithm.tsx
@@ -7,8 +7,70 @@ import Seo from "../components/seo"
 const CreateAlgorithm = () => (
   <Layout>
     <Seo title="AutoStock" />
-    <h3>Create your Algo</h3>
-    <h1>Content</h1>
+    <h2>Create Algorithm</h2>
+
+    <form>
+      <div className="form-group">
+        <label>Algorithm Name </label>
+        <input className="form-control" id="algoName" placeholder="Name your Algorithm"></input>
+      </div>
+      <div className="form-group">
+        <label>Stock Ticker Symbol </label>
+        <input className="form-control" id="tickerSymbol" placeholder="Eg. AAPL, TSLA"></input>
+      </div>
+      <div className="form-check">
+        <label>Time Inteveral </label>
+        <select className="form-control">
+          <option>1 Hour</option>
+        </select>
+      </div>
+      <div className="form-check">
+        <label>Indicator </label>
+        <select className="form-control">
+          <option>Simple Moving Average (SMA)</option>
+        </select>
+      </div>
+      <div className="form-check">
+        <label>Period 1 </label>
+        <select className="form-control">
+          <option>(close) 20</option>
+        </select>
+      </div>
+      <div className="form-check">
+        <label>Comparator </label>
+        <select className="form-control">
+          <option>Goes Above</option>
+          <option>Goes Below</option>
+        </select>
+      </div>
+      <div className="form-check">
+        <label>Period 2 </label>
+        <select className="form-control">
+          <option>(close) 50</option>
+        </select>
+      </div>
+      <div className="form-check">
+        <label>Action </label>
+        <select className="form-control">
+          <option>Buy</option>
+          <option>Sell</option>
+        </select>
+      </div>
+      <div className="form-check">
+        <label>Algorithm Running Time </label>
+        <select className="form-control">
+          <option>14 Days (a fortnite)</option>
+        </select>
+      </div>
+      
+      <button type="submit" className="btn btn-primary">Save and Backtest Algorithm</button>
+      <button type="submit" className="btn btn-primary">Share</button>
+
+    </form>
+
+
+
+
   </Layout>
 )
 
