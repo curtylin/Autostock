3680c234ba2732d286e99fd44d1e88d0f78df5fe
u1116418@utah.edu <curtis@Curtis-PC2.localdomain>
2021-12-01 03:31:27 -0700

    CRUD test should be fine. Just need to wait for firebase to be set up and the functions to be done before we can call them in the tests/db/algorithms.

--- AutoStock-React/tests/db/algorithms.py
+++ AutoStock-React/tests/db/algorithms.py
@@ -1,27 +1,10 @@
-#Create algorithms into database
-def create():
-    return True
-
-
-#Edit algorithms into database
-def edit():
-    return True
-
-
-#Check if algorithm saved properly
-def check():
-    return True
-
-
-#Delete algorithm from database
-def delete():
-    return True
+#import api. as algo
 
 def main():
-    check() #Check if empty
-    create()
-    check() #Check if created
-    edit()  
-    check() #Check if it was edited
-    delete()
-    check() #Check if deleted
+    algo.get() #Check if not present.
+    algo.create()
+    algo.get() #Check if created
+    algo.edit()  
+    algo.get() #Check if it was edited
+    algo.delete()
+    algo.get() #Check if deleted
