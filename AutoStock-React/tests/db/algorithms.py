831617446547adadec50744f8c628155bb9d3886
u1116418@utah.edu <curtis@Curtis-PC2.localdomain>
2021-12-01 18:41:18 -0700

    added dummy data. subject to change with the front end.

--- AutoStock-React/tests/db/algorithms.py
+++ AutoStock-React/tests/db/algorithms.py
@@ -1,4 +1,19 @@
-#import api. as algo
+import api.api as algo
+
+
+#TODO data structuring subject to change. 
+data = {
+    "name" : "algo1",
+    "ticker" : "AAPL",
+    "interval" : 1.0,
+    "indicator" : "SMA",
+    "period1" : 20,
+    "comparator" : "above",
+    "period2" : 50,
+    "action" : "BUY",
+    "runtime" : 14
+}
+
 
 def main():
     algo.get() #Check if not present.
