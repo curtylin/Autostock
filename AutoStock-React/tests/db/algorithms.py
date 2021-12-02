ceae56841395213b9fc612b7551c82dc13a2cdb2
u1116418@utah.edu <curtis@Curtis-PC2.localdomain>
2021-12-01 19:00:51 -0700

    added the scaffold ish code.. needs to be adapted to algorithms

--- AutoStock-React/tests/db/algorithms.py
+++ AutoStock-React/tests/db/algorithms.py
@@ -4,20 +4,22 @@ import api.api as algo
 #TODO data structuring subject to change. 
 data = {
     "name" : "algo1",
+    "userID" : "AaOU7E4YP6aTcWlaJK4u2XgRBQv1",
     "ticker" : "AAPL",
     "interval" : 1.0,
     "indicator" : "SMA",
-    "period1" : 20,
+    "period1" : "(close) 20",
     "comparator" : "above",
-    "period2" : 50,
-    "action" : "BUY",
-    "runtime" : 14
+    "period2" : "(close) 50",
+    "action" : "buy",
+    "runtime" : 14,
+    "public" : False
 }
 
 
 def main():
     algo.get() #Check if not present.
-    algo.create()
+    algo.create(data)
     algo.get() #Check if created
     algo.edit()  
     algo.get() #Check if it was edited
