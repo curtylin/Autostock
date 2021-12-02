ec979e1c496bef60309451a0971b14fed7f2f10a
curtylin <curtylin@gmail.com>
2021-12-01 19:00:51 -0700

    added the scaffold ish code.. needs to be adapted to algorithms

--- AutoStock-React/api/api.py
+++ AutoStock-React/api/api.py
@@ -1,6 +1,13 @@
 from flask import Flask 
+import firebase_admin
+from firebase_admin import credentials, firestore, initialize_app
 
 app = Flask(__name__, static_folder="../build", static_url_path="/")
+
+cred = credentials.Certificate("firestore_apikey.json")
+firebase_admin.initialize_app(cred)
+db = firestore.client()
+
 
 @app.errorhandler(404)
 def not_found(error):
@@ -10,6 +17,68 @@ def not_found(error):
 def index():
     return app.send_static_file('index.html')
 
-@app.route('/poggers')
-def poggersTest():
+
+@app.route('/addAlgorithm', methods=['POST'])
+def createAlgorithm(data):
+    await db.collection("algorithms").document("one").set(data)
     return 'True'
+
+@app.route('/add', methods=['POST'])
+def create():
+    """
+        create() : Add document to Firestore collection with request body.
+        Ensure you pass a custom ID as part of json body in post request,
+        e.g. json={'id': '1', 'title': 'Write a blog post'}
+    """
+    try:
+        id = request.json['id']
+        todo_ref.document(id).set(request.json)
+        return jsonify({"success": True}), 200
+    except Exception as e:
+        return f"An Error Occured: {e}"
+
+@app.route('/listAlgorithms', methods=['GET'])
+def read():
+    """
+        read() : Fetches documents from Firestore collection as JSON.
+        todo : Return document that matches query ID.
+        all_todos : Return all documents.
+    """
+    try:
+        # Check if ID was passed to URL query
+        todo_id = request.args.get('id')
+        if todo_id:
+            todo = todo_ref.document(todo_id).get()
+            return jsonify(todo.to_dict()), 200
+        else:
+            all_todos = [doc.to_dict() for doc in todo_ref.stream()]
+            return jsonify(all_todos), 200
+    except Exception as e:
+        return f"An Error Occured: {e}
+
+@app.route('/update', methods=['POST', 'PUT'])
+def update():
+    """
+        update() : Update document in Firestore collection with request body.
+        Ensure you pass a custom ID as part of json body in post request,
+        e.g. json={'id': '1', 'title': 'Write a blog post today'}
+    """
+    try:
+        id = request.json['id']
+        todo_ref.document(id).update(request.json)
+        return jsonify({"success": True}), 200
+    except Exception as e:
+        return f"An Error Occured: {e}"
+
+@app.route('/delete', methods=['GET', 'DELETE'])
+def delete():
+    """
+        delete() : Delete a document from Firestore collection.
+    """
+    try:
+        # Check for ID in URL query
+        todo_id = request.args.get('id')
+        todo_ref.document(todo_id).delete()
+        return jsonify({"success": True}), 200
+    except Exception as e:
+        return f"An Error Occured: {e}"
