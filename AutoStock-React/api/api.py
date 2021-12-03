d1fb52504a9b6a8958cc08f430f74df3cc89e1b9
Albert_Zhang <albertzhang8@DESKTOP-O7IMNR7.localdomain>
2021-12-02 21:22:02 -0700

    Added user CRUD starter code from the Google firebase tutorial, make sure this works very early prototype

--- AutoStock-React/api/api.py
+++ AutoStock-React/api/api.py
@@ -8,6 +8,7 @@ cred = credentials.Certificate("firestore_apikey.json")
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 algorithms_ref = db.collection('algorithms')
+users_ref = db.collection('users')
 
 
 @app.errorhandler(404)
@@ -28,7 +29,7 @@ def createAlgorithm(data):
 ## Start CRUD algorithm block
 ## Source code from: https://cloud.google.com/community/tutorials/building-flask-api-with-cloud-firestore-and-deploying-to-cloud-run 
 @app.route('/create', methods=['POST'])
-def create():
+def algo_create():
     """
         create() : Add document to Firestore collection with request body.
         Ensure you pass a custom ID as part of json body in post request,
@@ -42,7 +43,7 @@ def create():
         return f"An Error Occured: {e}"
 
 @app.route('/listAlgorithms', methods=['GET'])
-def read():
+def algo_read():
     """
         read() : Fetches documents from Firestore collection as JSON.
         todo : Return document that matches query ID.
@@ -61,7 +62,7 @@ def read():
         return f"An Error Occured: {e}
 
 @app.route('/update', methods=['POST', 'PUT'])
-def update():
+def algo_update():
     """
         update() : Update document in Firestore collection with request body.
         Ensure you pass a custom ID as part of json body in post request,
@@ -75,7 +76,7 @@ def update():
         return f"An Error Occured: {e}"
 
 @app.route('/delete', methods=['GET', 'DELETE'])
-def delete():
+def algo_delete():
     """
         delete() : Delete a document from Firestore collection.
     """
@@ -89,3 +90,68 @@ def delete():
 
 
 ## End algo CRUD Block
+
+
+# Start CRUD User block
+## Source code from: https://cloud.google.com/community/tutorials/building-flask-api-with-cloud-firestore-and-deploying-to-cloud-run 
+@app.route('/user-create', methods=['POST'])
+def user_create():
+    """
+        create() : Add document to Firestore collection with request body.
+        Ensure you pass a custom ID as part of json body in post request,
+        e.g. json={'id': '1', 'title': 'Write a blog post'}
+    """
+    try:
+        id = request.json['id']
+        users_ref.document(id).set(request.json)
+        return jsonify({"success": True}), 200
+    except Exception as e:
+        return f"An Error Occured: {e}"
+
+@app.route('/list-user', methods=['GET'])
+def user_read():
+    """
+        read() : Fetches documents from Firestore collection as JSON.
+        todo : Return document that matches query ID.
+        all_todos : Return all documents.
+    """
+    try:
+        # Check if ID was passed to URL query
+        user_id = request.args.get('id')
+        if user_id:
+            user = users_ref.document(user_id).get()
+            return jsonify(users_ref.to_dict()), 200
+        else:
+            user = [doc.to_dict() for doc in user.stream()]
+            return jsonify(user), 200
+    except Exception as e:
+        return f"An Error Occured: {e}
+
+@app.route('/user-update', methods=['POST', 'PUT'])
+def user_update():
+    """
+        update() : Update document in Firestore collection with request body.
+        Ensure you pass a custom ID as part of json body in post request,
+        e.g. json={'id': '1', 'title': 'Write a blog post today'}
+    """
+    try:
+        id = request.json['id']
+        users_ref.document(id).update(request.json)
+        return jsonify({"success": True}), 200
+    except Exception as e:
+        return f"An Error Occured: {e}"
+
+@app.route('/user-delete', methods=['GET', 'DELETE'])
+def user_delete():
+    """
+        delete() : Delete a document from Firestore collection.
+    """
+    try:
+        # Check for ID in URL query
+        user_id = request.args.get('id')
+        users_ref.document(user_id).delete()
+        return jsonify({"success": True}), 200
+    except Exception as e:
+        return f"An Error Occured: {e}"
+
+#End User Block
