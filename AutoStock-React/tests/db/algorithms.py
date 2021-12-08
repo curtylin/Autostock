095fc18a1d112efef840ca09cf40828eae896773
u1116418@utah.edu <curtis@Curtis-PC2.localdomain>
2021-12-07 17:11:22 -0700

    tweaked the create algorithm function to include the whole data instead of just the IDs. Cleaned up api.py, added a sample URL to test the api.. does not look like it's working though

--- AutoStock-React/tests/db/algorithms.py
+++ AutoStock-React/tests/db/algorithms.py
@@ -1,6 +1,7 @@
 import api.api as api
 
 #TODO data structuring subject to change. 
+# sample URL = http://localhost:5000/create-algorithm?data=%7B%22name%22%3A%22algo2%22%2C%22userID%22%3A%22AaOU7E4YP6aTcWlaJK4u2XgRBQv1%22%2C%22ticker%22%3A%22TSLA%22%2C%22interval%22%3A%222%22%2C%22indicator%22%3A%22SMA%22%2C%22period1%22%3A%22(close) 20%22%2C%22comparator%22%3A%22above%22%2C%22period2%22%3A%22(close) 50%22%2C%22action%22%3A%22buy%22%2C%22runtime%22%3A%2214%22%2C%22public%22%3A%22false%22%7D
 data = {
     "name" : "algo1",
     "userID" : "AaOU7E4YP6aTcWlaJK4u2XgRBQv1",
