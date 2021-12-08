9d076d5025f12cb341e52ea3fbe5d7d89f6e7c0e
u1116418@utah.edu <curtis@Curtis-PC2.localdomain>
2021-12-07 18:55:35 -0700

    reading algos finally works. two functions work- no passing id and by user id

--- AutoStock-React/api/api.py
+++ AutoStock-React/api/api.py
@@ -1,26 +1,31 @@
-from flask import Flask 
+from flask import Flask , request, jsonify
 import firebase_admin
 from firebase_admin import credentials, firestore, initialize_app
 
 app = Flask(__name__, static_folder="../build", static_url_path="/")
 
 cred = credentials.Certificate("firestore_apikey.json")
-firebase_admin.initialize_app(cred)
+# firebase_admin.initialize_app(cred)
+default_app = initialize_app(cred)
 db = firestore.client()
 algorithms_ref = db.collection('algorithms')
 
 @app.errorhandler(404)
 def not_found(error):
-    return app.send_static_file('index.html')
+    # return app.send_static_file('index.html')
+    return error
 
 @app.route('/')
 def index():
     return app.send_static_file('index.html')
 
+@app.route('/test')
+def test():
+    return "this works"
+
 ## Start CRUD algorithm block
 ## Source code from: https://cloud.google.com/community/tutorials/building-flask-api-with-cloud-firestore-and-deploying-to-cloud-run 
 ## https://dev.to/alexmercedcoder/basics-of-building-a-crud-api-with-flask-or-fastapi-4h70
-## NOTE: for POST and GET requests, you need to pass in the data into the URL: https://stackoverflow.com/questions/27577922/how-to-pass-a-json-array-as-a-parameter-in-url 
 @app.route('/create-algorithm', methods=['POST'])
 def algo_create():
     """
@@ -30,7 +35,7 @@ def algo_create():
     """
     try:
         data = request.json
-        print(data)
+        # print(data)
         algorithms_ref.document(data).set(request.json)
         return jsonify({"success": True}), 200
     except Exception as e:
@@ -45,12 +50,32 @@ def algo_read():
     """
     try:
         # Check if ID was passed to URL query
-        user_id = request.args.get('id')
-        if user_id:
-            algorithms = algorithms_ref.document(user_id).get()
-            return jsonify(algorithms.to_dict()), 200
+        id = request.args.get('id')
+        if id:
+            algorithms = algorithms_ref.document(userID).get()
+            return jsonify(todo.to_dict()), 200
         else:
-            algorithms = [doc.to_dict() for doc in algorithms.stream()]
+            algorithms = [doc.to_dict() for doc in algorithms_ref.stream()]
+            return jsonify(algorithms), 200
+    except Exception as e:
+        return f"An Error Occured: {e}"
+
+@app.route('/list-algorithm/<id>', methods=['GET'])
+def algo_read_user_id(id):
+    """
+        read() : Fetches documents from Firestore collection as JSON.
+        todo : Return document that matches query ID.
+        all_todos : Return all documents.
+    """
+    try:
+        # Check if ID was passed to URL query
+        # id = request.args.get('id')
+        if id:
+            userID = id
+            algorithms = [doc.to_dict() for doc in algorithms_ref.where("userID", "==", userID).stream()]
+            return jsonify(algorithms), 200
+        else:
+            algorithms = [doc.to_dict() for doc in algorithms_ref.stream()]
             return jsonify(algorithms), 200
     except Exception as e:
         return f"An Error Occured: {e}"
