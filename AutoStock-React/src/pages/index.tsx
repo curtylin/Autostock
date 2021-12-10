8938fa46b01616ef05dd76025f61726c7052665e
rftf <u1025269@utah.edu>
2021-12-10 00:19:43 -0700

    Enhanced the home page

--- AutoStock-React/src/pages/index.tsx
+++ AutoStock-React/src/pages/index.tsx
@@ -1,19 +1,47 @@
 import * as React from "react"
-import { Link } from "gatsby"
+import {Link} from "gatsby"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <Seo title="AutoStock" />
-    <Link to='/CreateAlgorithm'>Create New Algorithm</Link>
-    <div></div>
-    <Link to='/MyAlgorithm'>My Algorithms</Link>
-    <div></div>
-    <Link to='/publicAlgorithms'> Public Algorithms</Link>
-    <div></div>
-  </Layout>
-)
+import JSConfetti from 'js-confetti'
+
+const jsConfetti = new JSConfetti()
+
+
+const IndexPage = () => {
+
+    confetti();
+
+    return (
+        <Layout>
+            <Seo title="AutoStock"/>
+            <Link to='/CreateAlgorithm'>Create New Algorithm</Link>
+            <div></div>
+            <Link to='/MyAlgorithm'>My Algorithms</Link>
+            <div></div>
+            <Link to='/publicAlgorithms'> Public Algorithms</Link>
+            <div></div>
+        </Layout>
+    )
+}
+
+function confetti() {
+    for(var i = 0; i < 1000; i++) {
+        (function(index) {
+            setTimeout(function() {
+                jsConfetti.addConfetti({
+                    // emojis: ['🌈', '⚡️', '💥', '✨', '💫', '🌸'],
+                    emojis: ['😂','🍆','📈','👌','💦','🍑','💯'],
+                    // emojiSize: 100,
+
+                })
+                jsConfetti.addConfetti();
+
+                }, index*1000);
+        })(i);
+    }
+}
+
 
 export default IndexPage
