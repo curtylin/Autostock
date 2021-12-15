ff216c443367dff1520759aaeeafe1ea43282ab8
Albert_Zhang <u0852249@utah.edu>
2021-12-15 00:53:28 -0700

    Added fake data from competition to dashboard, also got rid of confetti and original links

--- AutoStock-React/src/pages/index.tsx
+++ AutoStock-React/src/pages/index.tsx
@@ -1,47 +1,138 @@
 import * as React from "react"
-import {Link} from "gatsby"
+import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-import JSConfetti from 'js-confetti'
+import JSConfetti from "js-confetti"
 
-const jsConfetti = new JSConfetti()
+import Box from "@mui/material/Box"
+import Card from "@mui/material/Card"
 
+import CardActions from "@mui/material/CardActions"
+import CardContent from "@mui/material/CardContent"
+import Button from "@mui/material/Button"
+import Typography from "@mui/material/Typography"
+import { GridItem } from "@chakra-ui/react"
+import Paper from "@mui/material/Paper"
+import Grid from "@mui/material/Grid"
+import { styled } from "@mui/material/styles"
+
+//const jsConfetti = new JSConfetti()
 
 const IndexPage = () => {
+  //  confetti();
 
-    confetti();
-
-    return (
-        <Layout>
-            <Seo title="AutoStock"/>
-            <Link to='/CreateAlgorithm'>Create New Algorithm</Link>
+  return (
+    <Layout>
+      <Seo title="AutoStock" />
+      {/* <Link to='/CreateAlgorithm'>Create New Algorithm</Link>
             <div></div>
             <Link to='/MyAlgorithm'>My Algorithms</Link>
             <div></div>
             <Link to='/publicAlgorithms'> Public Algorithms</Link>
-            <div></div>
-        </Layout>
+            <div></div> */}
+      {/* <Generate_Comp1></Generate_Comp1>
+      <Generate_Comp2></Generate_Comp2> */}
+
+      <Grid container spacing={2}>
+        <Grid item xs={4}>
+          <Generate_Comp1></Generate_Comp1>
+        </Grid>
+        <Grid item xs={4}>
+         <Generate_Comp2></Generate_Comp2>
+        </Grid>
+        <Grid item xs={4}>
+          <Generate_Comp3></Generate_Comp3>
+        </Grid>
+      </Grid>
+    </Layout>
+  )
+}
+const Item = styled(Paper)(({ theme }) => ({
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+}))
+
+const Generate_Comp1 = () => {
+  return (
+    <Card sx={{ minWidth: 275 }}>
+      <CardContent>
+        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+          Compeition 5 Days
+        </Typography>
+        <Typography variant="h5" component="div">
+          AAPL
+        </Typography>
+        <Typography sx={{ mb: 1.5 }} color="text.secondary">
+          Starting Cost $1000
+        </Typography>
+        <Typography variant="body2">
+          2 Days Left to Enter   
+          <br />
+          {'"Think Different"'}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
+  )
+}
+
+const Generate_Comp2 = () => {
+  return (
+    <Card sx={{ minWidth: 275 }}>
+      <CardContent>
+        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+          Compeition 3 Days
+        </Typography>
+        <Typography variant="h5" component="div">
+          TSLA
+        </Typography>
+        <Typography sx={{ mb: 1.5 }} color="text.secondary">
+          Starting Cost $700
+        </Typography>
+        <Typography variant="body2">
+          4 Days Left To Enter
+          <br />
+          {'"To accelerate the advent of ...."'}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
+  )
+}
+
+
+const Generate_Comp3 = () => {
+    return (
+      <Card sx={{ minWidth: 275 }}>
+        <CardContent>
+          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+           Compeition 7 Days
+          </Typography>
+          <Typography variant="h5" component="div">
+            GME
+          </Typography>
+          <Typography sx={{ mb: 1.5 }} color="text.secondary">
+            Starting Cost $1500
+          </Typography>
+          <Typography variant="body2">
+            8 Days Left To Enter
+            <br />
+            {'"Power to the Player"'}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small">Learn More</Button>
+        </CardActions>
+      </Card>
     )
-}
-
-function confetti() {
-    for(var i = 0; i < 1000; i++) {
-        (function(index) {
-            setTimeout(function() {
-                jsConfetti.addConfetti({
-                    // emojis: ['🌈', '⚡️', '💥', '✨', '💫', '🌸'],
-                    emojis: ['😂','🍆','📈','👌','💦','🍑','💯'],
-                    // emojiSize: 100,
-
-                })
-                jsConfetti.addConfetti();
-
-                }, index*1000);
-        })(i);
-    }
-}
-
+  }
 
 export default IndexPage
