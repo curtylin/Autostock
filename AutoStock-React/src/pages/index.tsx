1f92bfa16ab6057567b23d50de7ee47a5ed5becd
Albert_Zhang <u0852249@utah.edu>
2021-12-15 01:14:18 -0700

    "Added high charts to the dashboard as well as game features"

--- AutoStock-React/src/pages/index.tsx
+++ AutoStock-React/src/pages/index.tsx
@@ -17,6 +17,8 @@ import { GridItem } from "@chakra-ui/react"
 import Paper from "@mui/material/Paper"
 import Grid from "@mui/material/Grid"
 import { styled } from "@mui/material/styles"
+import Highcharts from "highcharts/highstock"
+import HighchartsReact from "highcharts-react-official"
 
 //const jsConfetti = new JSConfetti()
 
@@ -46,9 +48,73 @@ const IndexPage = () => {
           <Generate_Comp3></Generate_Comp3>
         </Grid>
       </Grid>
+      
+      <div id="chart">
+                <HighchartsReact
+                    highcharts={Highcharts}
+                    constructorType={"stockChart"}
+                    options={options}
+                />
+            </div>
+      
     </Layout>
   )
 }
+
+var fakeData = [],
+    volume = [],
+    dataLength = 100,
+    i = 0;
+
+for (i; i < dataLength; i += 1) {
+    fakeData.push([
+        "Dec " + i, // the date
+        3 + Math.random(), // open
+        4 + Math.random(), // high
+        1 + Math.random(), // low
+        2 + Math.random() // close
+    ]);
+
+    volume.push([
+        "Dec " + i, // the date
+        i * Math.random() // the volume
+    ]);
+}
+
+const options = {
+    title: {
+        text: 'AAPL'
+    },
+    yAxis: [
+        {
+            height: "80%"
+        },
+        {
+            top: "100%",
+            height: "20%",
+            offset: 0
+        }
+    ],
+    xAxis: [
+        {
+            outerWidth: "100%"
+        }
+    ],
+
+    series: [
+        {
+            type: "line",
+            data: fakeData,
+            yAxis: 0
+        },
+        {
+            type: 'column',
+            id: 'aapl-volume',
+            name: 'AAPL Volume',
+            data: volume,
+            yAxis: 1
+        }]
+};
 const Item = styled(Paper)(({ theme }) => ({
   ...theme.typography.body2,
   padding: theme.spacing(1),
