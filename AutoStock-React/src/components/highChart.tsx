ef53f2cf52661e66ebc04b95564949f24587addc
BrandonNham <u1059350@utah.edu>
2022-01-14 14:39:07 -0700

    Created separate component for HighChart on Index

---
+++ AutoStock-React/src/components/highChart.tsx
@@ -0,0 +1,71 @@
+import * as React from "react"
+import Highcharts from "highcharts/highstock"
+import HighchartsReact from "highcharts-react-official"
+
+var fakeData = [],
+  volume = [],
+  dataLength = 100,
+  i = 0
+
+for (i; i < dataLength; i += 1) {
+  fakeData.push([
+    "Dec " + i, // the date
+    3 + Math.random(), // open
+    4 + Math.random(), // high
+    1 + Math.random(), // low
+    2 + Math.random(), // close
+  ])
+
+  volume.push([
+    "Dec " + i, // the date
+    i * Math.random(), // the volume
+  ])
+}
+
+const options = {
+    title: {
+      text: "AAPL",
+    },
+    yAxis: [
+      {
+        height: "80%",
+      },
+      {
+        top: "100%",
+        height: "20%",
+        offset: 0,
+      },
+    ],
+    xAxis: [
+      {
+        outerWidth: "100%",
+      },
+    ],
+  
+    series: [
+      {
+        type: "line",
+        data: fakeData,
+        yAxis: 0,
+      },
+      {
+        type: "column",
+        id: "aapl-volume",
+        name: "AAPL Volume",
+        data: volume,
+        yAxis: 1,
+      },
+    ],
+  }
+const HighChart = ({
+    // data
+  }: any) => {
+    return (
+        <HighchartsReact
+        highcharts={Highcharts}
+        constructorType={"stockChart"}
+        options={options}
+        />
+    )
+  }
+export default HighChart
