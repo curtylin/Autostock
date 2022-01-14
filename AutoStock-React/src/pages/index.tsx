45a3d3b76015c73d3ed9f981e6f3ecc7608ff836
BrandonNham <u1059350@utah.edu>
2022-01-14 14:39:07 -0700

    Created separate component for HighChart on Index

--- AutoStock-React/src/pages/index.tsx
+++ AutoStock-React/src/pages/index.tsx
@@ -10,6 +10,7 @@ import Highcharts from "highcharts/highstock"
 import HighchartsReact from "highcharts-react-official"
 // @ts-ignore
 import Darkmode from "darkmode-js"
+import HighChart from "../components/highChart"
 
 const darkModeOptions = {
   bottom: "64px", // default: '32px'
@@ -61,71 +62,72 @@ const IndexPage = () => {
       </Grid>
 
       <div id="chart">
-        <HighchartsReact
+        {/* <HighchartsReact
           highcharts={Highcharts}
           constructorType={"stockChart"}
           options={options}
-        />
+        /> */}
+        <HighChart/>
       </div>
     </Layout>
   )
 }
 
-var fakeData = [],
-  volume = [],
-  dataLength = 100,
-  i = 0
+// var fakeData = [],
+//   volume = [],
+//   dataLength = 100,
+//   i = 0
 
-for (i; i < dataLength; i += 1) {
-  fakeData.push([
-    "Dec " + i, // the date
-    3 + Math.random(), // open
-    4 + Math.random(), // high
-    1 + Math.random(), // low
-    2 + Math.random(), // close
-  ])
+// for (i; i < dataLength; i += 1) {
+//   fakeData.push([
+//     "Dec " + i, // the date
+//     3 + Math.random(), // open
+//     4 + Math.random(), // high
+//     1 + Math.random(), // low
+//     2 + Math.random(), // close
+//   ])
 
-  volume.push([
-    "Dec " + i, // the date
-    i * Math.random(), // the volume
-  ])
-}
+//   volume.push([
+//     "Dec " + i, // the date
+//     i * Math.random(), // the volume
+//   ])
+// }
 
-const options = {
-  title: {
-    text: "AAPL",
-  },
-  yAxis: [
-    {
-      height: "80%",
-    },
-    {
-      top: "100%",
-      height: "20%",
-      offset: 0,
-    },
-  ],
-  xAxis: [
-    {
-      outerWidth: "100%",
-    },
-  ],
+// const options = {
+//   title: {
+//     text: "AAPL",
+//   },
+//   yAxis: [
+//     {
+//       height: "80%",
+//     },
+//     {
+//       top: "100%",
+//       height: "20%",
+//       offset: 0,
+//     },
+//   ],
+//   xAxis: [
+//     {
+//       outerWidth: "100%",
+//     },
+//   ],
 
-  series: [
-    {
-      type: "line",
-      data: fakeData,
-      yAxis: 0,
-    },
-    {
-      type: "column",
-      id: "aapl-volume",
-      name: "AAPL Volume",
-      data: volume,
-      yAxis: 1,
-    },
-  ],
-}
+//   series: [
+//     {
+//       type: "line",
+//       data: fakeData,
+//       yAxis: 0,
+//     },
+//     {
+//       type: "column",
+//       id: "aapl-volume",
+//       name: "AAPL Volume",
+//       data: volume,
+//       yAxis: 1,
+//     },
+//   ],
+// }
 // @ts-ignore
 const Item = styled(Paper)(({ theme }) => ({
   ...theme.typography.body2,
