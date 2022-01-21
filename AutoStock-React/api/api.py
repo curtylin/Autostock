05e1c3b5b6649361dc709a21bc6adfceecf4a158
rftf <u1025269@utah.edu>
2022-01-20 19:07:18 -0700

    Initial GraphQL

--- AutoStock-React/api/api.py
+++ AutoStock-React/api/api.py
@@ -323,3 +323,20 @@ def comp_unregister_competition(id):
 
 ## End comp CRUD Block
 
+# GraphQL graphql_playground
+@app.route("/graphql", methods=["GETS"])
+def graphql_playground():
+    return PLAYGROUND_HTML, 200
+
+@app.route("/graphql", methods=["POST"])
+def graphql_server():
+    data = request.get_json()
+    success, result = graphql_sync(
+        schema,
+        data,
+        context_value=request,
+        debug=app.debug
+    )
+    status_code = 200 if success else 400
+    return jsonify(result), status_code
+
