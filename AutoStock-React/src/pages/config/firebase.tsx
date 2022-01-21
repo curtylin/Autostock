84c0ed53ac5675aba5eec3eda77279f58faaf89c
Albert_Zhang <u0852249@utah.edu>
2022-01-20 19:46:15 -0700

    Fixed some minor import errors

--- AutoStock-React/src/pages/config/firebase.tsx
+++ AutoStock-React/src/pages/config/firebase.tsx
@@ -1,13 +1,12 @@
-import firebase from 'firebase/app';
-import 'firebase/auth';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
 import config from './config';
 
 const Firebase = firebase.initializeApp(config.firebase);
 
 // Add or Remove authentification methods here.
 export const Providers = {
-    google: new firebase.auth.GoogleAuthProvider(),
-    facebook: new firebase.auth.FacebookAuthProvider(),
+    google: new firebase.auth.GoogleAuthProvider()
 };
 
 export const auth = firebase.auth();
