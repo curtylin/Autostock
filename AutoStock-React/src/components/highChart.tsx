bd62439d80a05d95bfc36ed1bcaf3dcd99e28556
BrandonNham <u1059350@utah.edu>
2022-01-22 14:04:30 -0700

    Added stock ticker to HighChart

--- AutoStock-React/src/components/highChart.tsx
+++ AutoStock-React/src/components/highChart.tsx
@@ -3,7 +3,12 @@ import Highcharts from "highcharts/highstock"
 import HighchartsReact from "highcharts-react-official"
 
 
-var fakeData = [],
+
+
+
+const HighChart = ({setChart}: // data
+any) => {
+  var fakeData = [],
   volume = [],
   dataLength = 100,
   i = 0
@@ -22,54 +27,49 @@ for (i; i < dataLength; i += 1) {
     i * Math.random(), // the volume
   ])
 }
-
-const ChartOptions = {
-  title: {
-    text: "AAPL",
-  },
-  yAxis: [
-    {
-      height: "80%",
+  const ChartOptions = {
+    title: {
+      text: setChart,
     },
-    {
-      top: "100%",
-      height: "20%",
-      offset: 0,
-    },
-  ],
-  xAxis: [
-    {
-      outerWidth: "100%",
-    },
-  ],
-
-  series: [
-    {
-      type: "line",
-      data: fakeData,
-      yAxis: 0,
-    },
-    {
-      type: "column",
-      id: "aapl-volume",
-      name: "AAPL Volume",
-      data: volume,
-      yAxis: 1,
-    },
-  ],
-}
-const HighChart = ({setChart}: // data
-any) => {
-  return (
+    yAxis: [
+      {
+        height: "80%",
+      },
+      {
+        top: "100%",
+        height: "20%",
+        offset: 0,
+      },
+    ],
+    xAxis: [
+      {
+        outerWidth: "100%",
+      },
+    ],
+  
+    series: [
+      {
+        type: "line",
+        data: fakeData,
+        yAxis: 0,
+      },
+      {
+        type: "column",
+        id: `${setChart}-volume`,
+        name: `${setChart} Volume`,
+        data: volume,
+        yAxis: 1,
+      },
+    ],
+  }
+  return (  
     <div>
-      <h3>{setChart}</h3>
       <HighchartsReact
         highcharts={Highcharts}
         constructorType={"stockChart"}
         options={ChartOptions}
       />    
     </div>
-    
   )
 }
 export default HighChart
