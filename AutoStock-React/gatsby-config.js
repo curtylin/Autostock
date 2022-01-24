dcdbb018cd92779b278fd32b35354eee89c877bf
Nick <nickmountz16@gmail.com>
2022-01-23 18:27:00 -0700

    implemented email login

--- AutoStock-React/gatsby-config.js
+++ AutoStock-React/gatsby-config.js
@@ -1,3 +1,7 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   siteMetadata: {
     title: `AutoStock`,
@@ -30,7 +34,7 @@ module.exports = {
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
-    },
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
