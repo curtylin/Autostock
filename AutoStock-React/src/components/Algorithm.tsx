704716f73f0115938f64cad00dc0f621cda548d6
Nick Mountz <nickmountz16@gmail.com>
2022-01-25 14:03:25 -0700

    implimented Private Routes/login and logout

--- AutoStock-React/src/components/Algorithm.tsx
+++ AutoStock-React/src/components/Algorithm.tsx
@@ -1,8 +1,7 @@
 import * as React from "react"
 
-
-import Layout from "../components/layout"
-import Seo from "../components/seo"
+import Layout from "./layout"
+import Seo from "./seo"
 
 const Algorithm = () => (
   <Layout>
