b9c65daeb103b387f837a69ebde7fc37904aff65
u1116418@utah.edu <curtis@Curtis-PC2.localdomain>
2022-03-14 16:30:49 -0600

    changed to low, high, open, close... just need the manual entering of numbers

--- AutoStock-React/src/components/screens/CreateAlgorithm.tsx
+++ AutoStock-React/src/components/screens/CreateAlgorithm.tsx
@@ -325,7 +325,10 @@ const CreateAlgorithm = () => {
               <MenuItem value="">
                 <em>None</em>
               </MenuItem>
-              <MenuItem value={"(close) 20"}>(close) 20</MenuItem>
+              <MenuItem value={"low"}>low</MenuItem>
+              <MenuItem value={"high"}>high</MenuItem>
+              <MenuItem value={"open"}>open</MenuItem>
+              <MenuItem value={"close"}>close</MenuItem>
             </Select>
           </Tooltip>
         </FormControl>
@@ -370,7 +373,10 @@ const CreateAlgorithm = () => {
               <MenuItem value="">
                 <em>None</em>
               </MenuItem>
-              <MenuItem value={"(close) 20"}>(close) 20</MenuItem>
+              <MenuItem value={"low"}>low</MenuItem>
+              <MenuItem value={"high"}>high</MenuItem>
+              <MenuItem value={"open"}>open</MenuItem>
+              <MenuItem value={"close"}>close</MenuItem>
             </Select>
           </Tooltip>
         </FormControl>
