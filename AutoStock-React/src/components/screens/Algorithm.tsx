cfadd558d7f41185de854ef4e9893bfe474486de
BrandonNham <u1059350@utah.edu>
2022-03-24 00:23:04 -0600

    updated algo details page

--- AutoStock-React/src/components/screens/Algorithm.tsx
+++ AutoStock-React/src/components/screens/Algorithm.tsx
@@ -3,17 +3,59 @@ import "./screens.css"
 
 import Layout from "../layout"
 import Seo from "../seo"
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import { Grid } from "@mui/material";
+import HighChart from "../highChart"
 
 const Algorithm = () => {
-
+  const [data , setStockData] = useState([])
   const [algorithm, setAlgorithm] = useState<any>([])
   const [username, setUsername] = useState("")
+
+  
   useEffect(() => {
     console.log(window.history.state)
     console.log(window.history.state.id)
+    
     getAlgoDB()
     getUserDB()
     console.log(algorithm)
+    let today = new Date().toISOString().slice(0, 10)
+    const d = new Date();
+    d.setFullYear(d.getFullYear()-1);
+    
+    let lastYear = d.toISOString().slice(0,10)
+
+    console.log(today)
+    console.log(lastYear)
+    console.log(algorithm.ticker)
+    
+    let body = `{
+      "ticker": "${algorithm.ticker}",
+      "startDate": "${lastYear}",
+      "endDate": "${today.toString()}"
+    }`
+    const headers = new Headers()
+    headers.append("content-type", "application/json")
+    let init = {
+      method: "POST",
+      headers,
+      body,
+    }
+
+    fetch("http://localhost:5000/gethighchartdata ", init)
+      .then(res => {
+        return res.json()
+      })
+      .then(result => {
+        setStockData(result)
+      })
+
   }, [])
 
   const getAlgoDB = () => {
@@ -58,14 +100,63 @@ const Algorithm = () => {
   return (
   <Layout>
     <Seo title="AutoStock" />
-    <h1 className="algo_name">{algorithm.name}</h1>
-    <h2>Ticker: {algorithm.ticker}</h2>
-    <h3>Time Interval: {algorithm.timeInterval >= 24 ? (<>{algorithm.timeInterval/24} Days</>) : (<>{algorithm.timeInterval} Hours</>)}</h3>
-    <h3>Indicator 1: {algorithm.indicator1}</h3>
-    <h3>Period 1: {algorithm.period1}      Comparator: {algorithm.comparator}      Period 2: {algorithm.period2}</h3>
-    <h3>Action: {algorithm.action}</h3>
-    <h2>Algorithm Runtime: {algorithm.runningTime} Days</h2>
-    <h3>Username: {username}</h3>
+
+
+        <Typography fontFamily="-apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif"
+            fontWeight="Bold"sx={{ marginTop: 5, fontSize: 40, fontWeight: "bold" }} textAlign="center" variant= "h1"  gutterBottom>
+          {algorithm.name}
+        </Typography>
+     
+        <Typography sx={{ marginBottom: 5, fontSize: 20}} fontFamily="-apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif"
+            fontWeight="medium" variant= "h2" textAlign="center" gutterBottom>
+          User: {username}
+        </Typography>
+
+        <Card sx={{marginBottom: 2,  minWidth: 275 }}>
+          <CardContent>
+            <Typography sx={{ fontSize: 20}} justifyContent="center" fontFamily="-apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif"
+                  fontWeight="medium" variant= "h2"  gutterBottom>
+              Ticker: <span className="stockTickName"> {algorithm.ticker}</span>
+              
+            </Typography>
+            <Typography sx={{ fontSize: 20}} justifyContent="center" fontFamily="-apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif"
+                  fontWeight="medium" variant= "h2"  gutterBottom>
+              Time Interval: {algorithm.timeInterval >= 24 ? (<>{algorithm.timeInterval/24} Days</>) : (<>{algorithm.timeInterval} Hours</>)}
+            </Typography>
+            <Typography sx={{ fontSize: 20}} justifyContent="center" fontFamily="-apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif"
+                  fontWeight="medium" variant= "h2"  gutterBottom>
+              Algorithm Runtime: {algorithm.runningTime} Days            
+            </Typography>
+          </CardContent> 
+        </Card>
+
+      <Card sx={{marginBottom: 2,  minWidth: 275 }}>
+          <CardContent>
+            <Typography sx={{ fontSize: 20}} justifyContent="center" fontFamily="-apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif"
+                  fontWeight="medium" variant= "h2"  gutterBottom>
+              Indicator 1: <span className="dataToRight">{algorithm.indicator1}</span>
+            </Typography>
+            <Typography sx={{ fontSize: 20}} justifyContent="center" fontFamily="-apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif"
+                  fontWeight="medium" variant= "h2"  gutterBottom>
+              Period 1: {algorithm.period1}
+            </Typography>
+            <Typography sx={{ fontSize: 20}} justifyContent="center" fontFamily="-apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif"
+                  fontWeight="medium" variant= "h2"  gutterBottom>
+              Comparator: {algorithm.comparator}          
+            </Typography>
+            <Typography sx={{ fontSize: 20}} justifyContent="center" fontFamily="-apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif"
+                  fontWeight="medium" variant= "h2"  gutterBottom>
+              Period 2: {algorithm.period2}            
+            </Typography>
+            <Typography sx={{ fontSize: 20}} justifyContent="center" fontFamily="-apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif"
+                  fontWeight="medium" variant= "h2"  gutterBottom>
+              Action: {algorithm.action}
+            </Typography>
+          </CardContent> 
+        </Card>
+
+        <HighChart stock={algorithm.ticker} stockData={data}/>
+  
   </Layout>
   )
 }
