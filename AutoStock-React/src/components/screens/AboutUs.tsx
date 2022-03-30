821416201ce27aff4a6495388ca159ad64db8a49
u1116418@utah.edu <curtis@Curtis-PC2.localdomain>
2022-03-30 11:57:19 -0600

    added user bios

--- AutoStock-React/src/components/screens/AboutUs.tsx
+++ AutoStock-React/src/components/screens/AboutUs.tsx
@@ -20,7 +20,7 @@ const AboutUs = () => {
             <br></br>
 
             <h1>Our Story</h1>
-            <p>insert who we are</p>
+            <p>We are currently in our final year of completion of the Computer Science B.S. Program at the University of Utah. This is our capstone project. We hope you like it, we do.</p>
             <br></br>
 
             <h1>Our Mission</h1>
@@ -31,7 +31,34 @@ const AboutUs = () => {
             <br></br>
 
             <h1>Our Team</h1>
-            <p>insert user Bios</p>
+
+            <hr></hr>
+            <img src="https://media-exp1.licdn.com/dms/image/C4E03AQGi-_LJJPb3Uw/profile-displayphoto-shrink_200_200/0/1587842235640?e=1654128000&v=beta&t=mISNSvknyptico7TEqa9hth2e4s98q2HxmInKCZaS8o" alt="Albert Zhang portrait" width="250"></img>
+            <h2>Albert Zhang</h2>
+            <p>Albert's BIO HERE</p>
+
+            <hr></hr>
+            <img src="https://media-exp1.licdn.com/dms/image/C4D03AQGa2YqdqowjTg/profile-displayphoto-shrink_200_200/0/1626983303468?e=1654128000&v=beta&t=BbviMuehzZhurC__h_R9HOiyZQx4RIL_ku1yGUqgXt4" alt="Brandon Nham portrait" width="250"></img>
+            <h2>Brandon Nham</h2>
+            <p>Brandon's BIO HERE</p>
+
+            <hr></hr>
+            <img src="https://media-exp1.licdn.com/dms/image/C5603AQHwwK5bJU13AA/profile-displayphoto-shrink_200_200/0/1591376666560?e=1654128000&v=beta&t=7YdhOkopgDn31-WMuTt6bkYEcnG2xM0c2PM0Ne710-o" alt="Curtis Lin portrait" width="250"></img>
+            <h2>Curtis Lin</h2>
+            <p>Hi! I’m Curtis! For this project, I was in charge of full stack, more specifically, anything relating to the database. I can be reached on <a href="www.linkedin.com/in/curtis-lin">LinkedIn</a>.</p>
+
+            <hr></hr>
+            <img src="https://firebasestorage.googleapis.com/v0/b/autostock-fef22.appspot.com/o/jony.png?alt=media&token=d99b5016-45cc-4b4f-90cd-eba7dc57f37f" alt="Jonathan Fairbanks portrait" width="250"></img>
+            <h2>Jonathan Fairbanks</h2>
+            <p>Jonathan's BIO HERE</p>
+
+            <hr></hr>
+            <img src="https://media-exp1.licdn.com/dms/image/C5603AQGxsLrwpvSVyA/profile-displayphoto-shrink_200_200/0/1621444830519?e=1654128000&v=beta&t=0za5UikHUTBDNvYiZY39Hsi7rN9nGz3Kgvj1c0FvBho" alt="Nick Mountz portrait" width="250"></img>
+            <h2>Nick Mountz</h2>
+            <p>Nick's BIO HERE</p>
+
+
+
             <br></br>
 
 
