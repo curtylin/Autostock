cbb622ace93cef58176cd8a790bcb178b0db7522
curtylin <curtylin@gmail.com>
2022-04-03 01:28:53 -0600

    added photo with steps for SMA... fixed some more formatting

--- AutoStock-React/src/components/screens/Quickstartalgo.tsx
+++ AutoStock-React/src/components/screens/Quickstartalgo.tsx
@@ -9,10 +9,10 @@ const QuickStartGuide = () => {
         <Layout>
             <h1>I want to create an algorithm... where should I start?</h1>
             <br></br>
+            <img src="https://i.imgur.com/ZGZ7EXP.png"></img>
 
             <h2>1. Name your algorithm</h2>
             <p>We recommend naming it something with your name, ticker, and indicator with it. </p>
-            {/* <img src="https://i.imgur.com/vYtuZIC.gif"></img> */}
             <br></br>
 
             <h2>2. Pick a stock ticker you would want to test your algorithm on.</h2>
@@ -35,17 +35,16 @@ const QuickStartGuide = () => {
                 Below are some links to some basic indicators: 
                 <br></br>
                 <br></br>
-                <Link to="https://www.investopedia.com/terms/s/sma.asp">SMA, or Simple moving average</Link> is the most basic indicator you can have. It essentially calculates the average of a selected range.
+                In the example above, we selected <Link to="https://www.investopedia.com/terms/s/sma.asp">SMA, or Simple moving average</Link>. It is the most basic indicator you can have. It essentially calculates the average of a selected range.
                 <br></br>
                 <Link to="https://www.investopedia.com/articles/trading/07/adx-trend-indicator.asp">ADX, or Average Directional Index Rating</Link> is a more advanced indicator that can be used to determine if the market is trending up or down.
                 <br></br>
                 The list goes on and on.
                 
             </p>
-            {/* <img src="https://i.imgur.com/vYtuZIC.gif"></img> */}
             <br></br>
 
-            <h2>4. Select the Period 1.</h2>
+            <h2>5. Select the Period 1.</h2>
             <p>                While different indicators take in different inputs, the most common indicators belong to the moving average family. Moving averages take in an input
                 known as periods. The smaller the period the more sensative it will be to price changes. 
                 For Moving Averages a period will average the prices for the input period ammount and add the price on the previous day. 
@@ -57,54 +56,47 @@ const QuickStartGuide = () => {
 
 
             
-            {/* <img src="https://i.imgur.com/vYtuZIC.gif"></img> */}
             <br></br>
 
-            <h2>5. Select the Indicator 2/Comparator.</h2>
-            <p>Next portion of the entry is to give it another indicator or strategy to compare to. This will determine the different entry rules on when to buy and sell</p>
+            <h2>6. Select the Indicator 2/Comparator.</h2>
+            <p>Next portion of the entry is to give it another indicator or strategy to compare to. This will determine the different entry rules on when to buy and sell. <br></br>
+            In the example, we have it so that we are comparing the 20 day open average and the 10 day open average. In other words, if the 20 day open average is higher than the 10 day open, we will execute a buy order. </p>
 
                 
-            {/* <img src="https://i.imgur.com/vYtuZIC.gif"></img> */}
             <br></br>
 
-            <h2>6. Select the Period 2.</h2>
+            <h2>7. Select the Period 2.</h2>
             <p> Similar to period 1, Period 2 works as the period for the second indicator to complete the entry rules. </p>
                
 
 
-            {/* <img src="https://i.imgur.com/vYtuZIC.gif"></img> */}
             <br></br>
 
-            <h2>7. Select Buy or Sell.</h2>
-            <p>You are given an action that will perform when the conditions that are meant in the Entry/Entries are met. 
-                For more information about buy and sell orders 
-                <Link to="https://www.investopedia.com/terms/s/sma.asp"></Link> For more information about buy and sell orders </p>
-            {/* <img src="https://i.imgur.com/vYtuZIC.gif"></img> */}
+            <h2>8. Select Buy or Sell.</h2>
+            <p>You are given an action that will perform when the conditions that are meant in the Entry/Entries are met. <br></br>
+
+                <Link to="https://www.investopedia.com/terms/s/sma.asp">For more information about buy and sell orders</Link>.  </p>
                 
 
-            <h2>8. Select the algorithm running time.</h2>
+            <h2>9. Select the algorithm running time.</h2>
             <p>This will determine how long your algorithm will run for. Strategies especially indicators will be more effective depending on the length ran for. </p>
 
                 
-            {/* <img src="https://i.imgur.com/vYtuZIC.gif"></img> */}
             <br></br>
 
-            <h2>9. Add additional paramters.</h2>
+            <h2>10. Add additional paramters.</h2>
             <p>You may chainlink different entries together in order to form a new strategy. This is not recommended for beginners and we recommend playing arround with one
                 entry first, but is encouraged for more advanced users. </p>
                 
 
-            {/* <img src="https://i.imgur.com/vYtuZIC.gif"></img> */}
             <br></br>
 
-            <h2>10. When you are somewhat happy with your algorithm, Backtest!</h2>
+            <h2>11. When you are somewhat happy with your algorithm, Backtest!</h2>
             <p>You don't have to fully be satisfied with the algorithm at this point. Backtest your algorithm on historical data to see how well it would've done. Afterwards, you can repeat steps 2-10 until you feel like you are satisfied. Not only will it tell you how much money your algorithm would've made, it also tells you where your buy/sell orders were executed by the algorithm. </p>
-            {/* <img src="https://i.imgur.com/vYtuZIC.gif"></img> */}
             <br></br>
 
-            <h2>11. Save your algorithm</h2>
+            <h2>12. Save your algorithm</h2>
             <p>After you have backtested and feel your algorithm is ready, make sure to save your algorithm! This would allow you to enter your algorithm into a competition!</p>
-            {/* <img src="https://i.imgur.com/vYtuZIC.gif"></img> */}
             <br></br>
 
             <h1>When you feel you are ready, let's <Link to="/app/createalgorithm">create an algorithm!</Link></h1>
