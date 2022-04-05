aa10d613c91428ddf7b1988826c5baeb1e37e450
BrandonNham <u1059350@utah.edu>
2022-04-05 11:51:48 -0600

    updated api calls to be less frequent

--- AutoStock-React/src/components/comments.tsx
+++ AutoStock-React/src/components/comments.tsx
@@ -9,38 +9,39 @@ import { useState, useEffect } from "react";
 const Comments = ({
     id,
     commentText,
-    commentUser
+    commentUser,
+    allUsers
 }: any) => {  
   const [users, setUsers] = useState(new Map<string, string>())
 
   useEffect(() => {
-    getUsersDB()
+    // getUsersDB()
   }, [])
 
-  const getUsersDB = () => {
-    //fetch post to localhost
-    fetch("http://localhost:5000/list-user", {
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      method: "GET",
-    })
-      .then(res => {
-        return res.json()
-      })
-      .then(result => {
-        for(let i = 0; i < result.length; i++){
-          setUsers(prev => new Map([...prev, [result[i].userID, result[i].username]]))
-        }
-      })
-  }
+  // const getUsersDB = () => {
+  //   //fetch post to localhost
+  //   fetch("http://localhost:5000/list-user", {
+  //     headers: {
+  //       Accept: "application/json",
+  //       "Content-Type": "application/json",
+  //     },
+  //     method: "GET",
+  //   })
+  //     .then(res => {
+  //       return res.json()
+  //     })
+  //     .then(result => {
+  //       for(let i = 0; i < result.length; i++){
+  //         setUsers(prev => new Map([...prev, [result[i].userID, result[i].username]]))
+  //       }
+  //     })
+  // }
 
   return (
     <div>
         <AccordionDetails sx={{ml:3}}>            
           <Typography fontSize="16px" fontWeight="300" variant="h5" component="div">
-            <span className="dis_UserName">{users.get(commentUser) == "" || !users.has(commentUser) ? "Anonymous" : users.get(commentUser)} </span>{commentText}
+            <span className="dis_UserName">{allUsers.get(commentUser) == "" || !allUsers.has(commentUser) ? "Anonymous" : allUsers.get(commentUser)} </span>{commentText}
           </Typography>  
         </AccordionDetails>
         
