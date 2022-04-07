635da7ec627f915fef143f61289c33b606936d5c
rftf <u1025269@utah.edu>
2022-04-07 01:52:42 -0600

    Linted entire project with prettier, as well as reformatted python file

--- AutoStock-React/src/components/layout_landing.tsx
+++ AutoStock-React/src/components/layout_landing.tsx
@@ -5,42 +5,36 @@
  * See: https://www.gatsbyjs.com/docs/use-static-query/
  */
 
- import * as React from "react"
- import { useStaticQuery, graphql } from "gatsby"
- 
- import Header from "./header"
- import "./layout_landing.css"
- 
- interface LayoutProps {
-   children: React.ReactNode
- }
- 
- const LayoutLanding = ({ children }: LayoutProps) => {
- 
-   const siteTitle: string = "Autostock"
-   
-   return (
-     <>
-       <Header siteTitle={siteTitle} />
-       <div
-        className="layout_body"
-         style={{
-         }}
-       >
-         <main >{children}</main>
-         <footer
-           style={{
-             marginTop: `2rem`,
-           }}
-         >
-           © {new Date().getFullYear()}
-           {` `}
-           <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">AutoStock</a>
-         </footer>
-       </div>
-     </>
-   )
- }
- 
- export default LayoutLanding
- 
+import * as React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+
+import Header from "./header"
+import "./layout_landing.css"
+
+interface LayoutProps {
+  children: React.ReactNode
+}
+
+const LayoutLanding = ({ children }: LayoutProps) => {
+  const siteTitle: string = "Autostock"
+
+  return (
+    <>
+      <Header siteTitle={siteTitle} />
+      <div className="layout_body" style={{}}>
+        <main>{children}</main>
+        <footer
+          style={{
+            marginTop: `2rem`,
+          }}
+        >
+          © {new Date().getFullYear()}
+          {` `}
+          <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">AutoStock</a>
+        </footer>
+      </div>
+    </>
+  )
+}
+
+export default LayoutLanding
--- AutoStock-React/src/components/screens/competitions.tsx
+++ AutoStock-React/src/components/screens/competitions.tsx
@@ -8,7 +8,6 @@ import Seo from "../seo"
 import { getUser } from "../../services/auth"
 import { navigate } from "gatsby"
 
-
 const Competitions = () => {
   const [competitions, setCompetitions] = useState([])
   const [enteredComps, setEnteredComps] = useState([])
@@ -23,7 +22,7 @@ const Competitions = () => {
         setCompetitions(result)
       })
 
-      fetch(`http://localhost:5000/list-entered-competitions/${getUser().uid}`)
+    fetch(`http://localhost:5000/list-entered-competitions/${getUser().uid}`)
       .then(res => {
         return res.json()
       })
@@ -31,7 +30,9 @@ const Competitions = () => {
         setEnteredComps(result)
       })
 
-      fetch(`http://localhost:5000/list-nonregisted-competitions/${getUser().uid}`)
+    fetch(
+      `http://localhost:5000/list-nonregisted-competitions/${getUser().uid}`
+    )
       .then(res => {
         return res.json()
       })
@@ -40,93 +41,158 @@ const Competitions = () => {
       })
   }, [])
 
-
   return (
     <Layout>
-        <Seo title="Autostock" />
-        <h1>Competitions</h1>
-        
-        <h3>Upcoming Competitions</h3>
-        <Grid direction={{xs:'column', md:'row'}} justifyContent="left" alignContent={{xs:'center',sm:'flex', md:'flex'}} container spacing={3} sx={{mb: 1}}>
-              {notEnteredComps.slice(0, 3).map((comp: any, index: number) => {
-              let cardProps = {
-                  compLength: comp.duration,
-                  compTicker: comp.name,
-                  compStartingVal: `Starting Balance: ${comp.startingBalance}`,
-                  compDeadline: comp.closeDate,
-                  description: comp.description,
-                  id: comp.id,
-                  logo: comp.logo,
-              }
-              return (
-                  <Grid key={index} item sm={12} md={4} lg={4} justifyContent="left">
-                      <ComplexCompCard key={index} {...cardProps} />
-                  </Grid>
-              )
-              })}
-          </Grid>
-          <Box sx={{ width: 1 }}>
-            <Button style={{ float: "right"}} sx={{mt:0, pt: 0}} className="mdc-button mdc-button--raised" onClick={event => {navigate(`/app/notenteredcompetitions`)}}> See More</Button>
-          </Box>
-            
-          <Box sx={{mt:3}}>
-            <h3>Your Competitions</h3>
-          </Box>
-          {enteredComps.length > 0 ?
-          <Grid direction={{xs:'column', md:'row'}} justifyContent="left" alignContent={{xs:'center',sm:'flex', md:'flex'}} container spacing={3} sx={{mb: 1}}>
-              {enteredComps.slice(0, 3).map((comp: any, index: number) => {
-              let cardProps = {
-                  compLength: comp.duration,
-                  compTicker: comp.name,
-                  compStartingVal: `Starting Balance: ${comp.startingBalance}`,
-                  compDeadline: comp.closeDate,
-                  description: comp.description,
-                  id: comp.id,
-                  logo: comp.logo,
-              }
-              return (
-                  <Grid key={index} item sm={12} md={4} lg={4} justifyContent="left">
-                      <ComplexCompCard key={index} {...cardProps} />
-                  </Grid>
-              )
-              })}
-          </Grid>
-          :
-          <Typography
-              sx={{mb: 5, ml: 5}}
+      <Seo title="Autostock" />
+      <h1>Competitions</h1>
+
+      <h3>Upcoming Competitions</h3>
+      <Grid
+        direction={{ xs: "column", md: "row" }}
+        justifyContent="left"
+        alignContent={{ xs: "center", sm: "flex", md: "flex" }}
+        container
+        spacing={3}
+        sx={{ mb: 1 }}
+      >
+        {notEnteredComps.slice(0, 3).map((comp: any, index: number) => {
+          let cardProps = {
+            compLength: comp.duration,
+            compTicker: comp.name,
+            compStartingVal: `Starting Balance: ${comp.startingBalance}`,
+            compDeadline: comp.closeDate,
+            description: comp.description,
+            id: comp.id,
+            logo: comp.logo,
+          }
+          return (
+            <Grid key={index} item sm={12} md={4} lg={4} justifyContent="left">
+              <ComplexCompCard key={index} {...cardProps} />
+            </Grid>
+          )
+        })}
+      </Grid>
+      <Box sx={{ width: 1 }}>
+        <Button
+          style={{ float: "right" }}
+          sx={{ mt: 0, pt: 0 }}
+          className="mdc-button mdc-button--raised"
+          onClick={event => {
+            navigate(`/app/notenteredcompetitions`)
+          }}
+        >
+          {" "}
+          See More
+        </Button>
+      </Box>
+
+      <Box sx={{ mt: 3 }}>
+        <h3>Your Competitions</h3>
+      </Box>
+      {enteredComps.length > 0 ? (
+        <Grid
+          direction={{ xs: "column", md: "row" }}
+          justifyContent="left"
+          alignContent={{ xs: "center", sm: "flex", md: "flex" }}
+          container
+          spacing={3}
+          sx={{ mb: 1 }}
+        >
+          {enteredComps.slice(0, 3).map((comp: any, index: number) => {
+            let cardProps = {
+              compLength: comp.duration,
+              compTicker: comp.name,
+              compStartingVal: `Starting Balance: ${comp.startingBalance}`,
+              compDeadline: comp.closeDate,
+              description: comp.description,
+              id: comp.id,
+              logo: comp.logo,
+            }
+            return (
+              <Grid
+                key={index}
+                item
+                sm={12}
+                md={4}
+                lg={4}
+                justifyContent="left"
+              >
+                <ComplexCompCard key={index} {...cardProps} />
+              </Grid>
+            )
+          })}
+        </Grid>
+      ) : (
+        <Typography sx={{ mb: 5, ml: 5 }}>
+          You have not entered any competitions yet.
+        </Typography>
+      )}
+      {enteredComps.length > 0 ? (
+        <Box sx={{ width: 1 }}>
+          {" "}
+          <Button
+            style={{ float: "right" }}
+            sx={{ mt: 0, pt: 0 }}
+            className="mdc-button mdc-button--raised"
+            onClick={event => {
+              navigate(`/app/enteredcompetitions`)
+            }}
           >
-            You have not entered any competitions yet.
-          </Typography>
-        }
-        {enteredComps.length > 0 ? <Box sx={{ width: 1 }}> <Button style={{ float: "right"}} sx={{mt:0, pt: 0}} className="mdc-button mdc-button--raised" onClick={event => {navigate(`/app/enteredcompetitions`)}}> See More</Button></Box>
- : null}
-        <Box sx={{mt:3}}>
-            <h3>All Competitions</h3>
-          </Box>
-        <Grid direction={{xs:'column', md:'row'}} justifyContent="left" alignContent={{xs:'center',sm:'flex', md:'flex'}} container spacing={3} sx={{mb: 1}}>
-              {competitions.slice(0, 3).map((comp: any, index: number) => {
-              let cardProps = {
-                  compLength: comp.duration,
-                  compTicker: comp.name,
-                  compStartingVal: `Starting Balance: ${comp.startingBalance}`,
-                  compDeadline: comp.closeDate,
-                  description: comp.description,
-                  id: comp.id,
-                  logo: comp.logo,
-              }
-              return (
-                  <Grid key={index} item sm={12} md={4} lg={4} justifyContent="center">
-                      <ComplexCompCard key={index} {...cardProps} />
-                  </Grid>
-              )
-              })}
-          </Grid>
-          <Box sx={{ width: 1 }}>
-            <Button style={{ float: "right"}} sx={{mt:0, pt: 0}} className="mdc-button mdc-button--raised" onClick={event => {navigate(`/app/allcompetitions`)}}> See More</Button>
-          </Box>    
-        </Layout>
+            {" "}
+            See More
+          </Button>
+        </Box>
+      ) : null}
+      <Box sx={{ mt: 3 }}>
+        <h3>All Competitions</h3>
+      </Box>
+      <Grid
+        direction={{ xs: "column", md: "row" }}
+        justifyContent="left"
+        alignContent={{ xs: "center", sm: "flex", md: "flex" }}
+        container
+        spacing={3}
+        sx={{ mb: 1 }}
+      >
+        {competitions.slice(0, 3).map((comp: any, index: number) => {
+          let cardProps = {
+            compLength: comp.duration,
+            compTicker: comp.name,
+            compStartingVal: `Starting Balance: ${comp.startingBalance}`,
+            compDeadline: comp.closeDate,
+            description: comp.description,
+            id: comp.id,
+            logo: comp.logo,
+          }
+          return (
+            <Grid
+              key={index}
+              item
+              sm={12}
+              md={4}
+              lg={4}
+              justifyContent="center"
+            >
+              <ComplexCompCard key={index} {...cardProps} />
+            </Grid>
+          )
+        })}
+      </Grid>
+      <Box sx={{ width: 1 }}>
+        <Button
+          style={{ float: "right" }}
+          sx={{ mt: 0, pt: 0 }}
+          className="mdc-button mdc-button--raised"
+          onClick={event => {
+            navigate(`/app/allcompetitions`)
+          }}
+        >
+          {" "}
+          See More
+        </Button>
+      </Box>
+    </Layout>
   )
 }
 
 export default Competitions
-
