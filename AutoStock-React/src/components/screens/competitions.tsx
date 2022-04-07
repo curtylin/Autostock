de3c76d8865ac1958741b8fd89ab018aef212965
BrandonNham <u1059350@utah.edu>
2022-04-07 00:13:49 -0600

    Added not entered, entered, all comps lists

--- AutoStock-React/src/components/screens/competitions.tsx
+++ AutoStock-React/src/components/screens/competitions.tsx
@@ -1,13 +1,17 @@
 import React, { useState, useEffect } from "react"
-import { Box, Grid } from "@mui/material"
+import { Box, Grid, Typography } from "@mui/material"
 import CompCard from "../compCard"
 import Layout from "../layout"
 import MediaCompCard from "../mediaCompCard"
 import ComplexCompCard from "../complexCompCard"
 import Seo from "../seo"
+import { getUser } from "../../services/auth"
+
 
 const Competitions = () => {
   const [competitions, setCompetitions] = useState([])
+  const [enteredComps, setEnteredComps] = useState([])
+  const [notEnteredComps, setNotEnteredCompetitions] = useState([])
 
   useEffect(() => {
     fetch("http://localhost:5000/list-competitions")
@@ -17,6 +21,22 @@ const Competitions = () => {
       .then(result => {
         setCompetitions(result)
       })
+
+      fetch(`http://localhost:5000/list-entered-competitions/${getUser().uid}`)
+      .then(res => {
+        return res.json()
+      })
+      .then(result => {
+        setEnteredComps(result)
+      })
+
+      fetch(`http://localhost:5000/list-nonregisted-competitions/${getUser().uid}`)
+      .then(res => {
+        return res.json()
+      })
+      .then(result => {
+        setNotEnteredCompetitions(result)
+      })
   }, [])
 
 
@@ -24,8 +44,10 @@ const Competitions = () => {
     <Layout>
         <Seo title="Autostock" />
         <h1>Competitions</h1>
-          <Grid direction={{xs:'column', md:'row'}} justifyContent="center" alignContent={{xs:'center',sm:'flex', md:'flex'}} container spacing={3} sx={{mb: 5}}>
-              {competitions.slice(0, 6).map((comp: any, index: number) => {
+
+        <h3>Upcoming Competitions</h3>
+        <Grid direction={{xs:'column', md:'row'}} justifyContent="center" alignContent={{xs:'center',sm:'flex', md:'flex'}} container spacing={3} sx={{mb: 5}}>
+              {notEnteredComps.slice(0, 3).map((comp: any, index: number) => {
               let cardProps = {
                   compLength: comp.duration,
                   compTicker: comp.name,
@@ -41,9 +63,54 @@ const Competitions = () => {
                   </Grid>
               )
               })}
-              
           </Grid>
-        
+          <h3>Your Competitions</h3>
+          {enteredComps.length > 0 ?
+          <Grid direction={{xs:'column', md:'row'}} justifyContent="center" alignContent={{xs:'center',sm:'flex', md:'flex'}} container spacing={3} sx={{mb: 5}}>
+              {enteredComps.slice(0, 3).map((comp: any, index: number) => {
+              let cardProps = {
+                  compLength: comp.duration,
+                  compTicker: comp.name,
+                  compStartingVal: `Starting Balance: ${comp.startingBalance}`,
+                  compDeadline: comp.closeDate,
+                  description: comp.description,
+                  id: comp.id,
+                  logo: comp.logo,
+              }
+              return (
+                  <Grid key={index} item sm={12} md={4} lg={4} justifyContent="center">
+                      <ComplexCompCard key={index} {...cardProps} />
+                  </Grid>
+              )
+              })}
+          </Grid>
+          :
+          <Typography
+              sx={{mb: 5, ml: 5}}
+          >
+            You have not entered any competitions yet.
+          </Typography>
+        }
+
+        <h3>All Competitions</h3>
+        <Grid direction={{xs:'column', md:'row'}} justifyContent="center" alignContent={{xs:'center',sm:'flex', md:'flex'}} container spacing={3} sx={{mb: 5}}>
+              {competitions.slice(0, 3).map((comp: any, index: number) => {
+              let cardProps = {
+                  compLength: comp.duration,
+                  compTicker: comp.name,
+                  compStartingVal: `Starting Balance: ${comp.startingBalance}`,
+                  compDeadline: comp.closeDate,
+                  description: comp.description,
+                  id: comp.id,
+                  logo: comp.logo,
+              }
+              return (
+                  <Grid key={index} item sm={12} md={4} lg={4} justifyContent="center">
+                      <ComplexCompCard key={index} {...cardProps} />
+                  </Grid>
+              )
+              })}
+          </Grid>
     </Layout>
   )
 }
