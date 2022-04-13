97bb193742a59f0fbf25162a18d2c9a31cf4858d
rftf <u1025269@utah.edu>
2022-04-12 21:07:46 -0600

    Recommendations table now on

--- AutoStock-React/src/components/data_components/Stockmajorholders.tsx
+++ AutoStock-React/src/components/data_components/Stockmajorholders.tsx
@@ -1,7 +1,29 @@
 import React from 'react'
+import { useEffect } from 'react'
+import { InternalPieCanvas } from './nivo/InternalPieCanvas'
 
 export default function Stockmajorholders({stock}: any) {
+
+  const [majorHolders, setMajorHolders] = React.useState([])
+  useEffect(() => {
+    if(stock !== '') {
+      fetch(`http://localhost:5000/getMajorHolders/${stock}`)
+        .then(res => res.json())
+        .then(result => {
+          setMajorHolders(result)
+          console.log(result)
+        })
+      }
+  },[stock])
+
+
   return (
-    <div>Stockmajorholders</div>
+    <div style={{position: 'relative'}}>
+      <div style={{position: 'absolute', width: '100%', height: '100%' }}>
+        <InternalPieCanvas data={majorHolders} />
+      </div>
+    </div>
+    // <div>{}</div>
   )
 }
+
---
+++ AutoStock-React/src/components/data_components/nivo/InternalPieCanvas.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { ResponsivePie } from '@nivo/pie'
+
+export const InternalPieCanvas = ({ data /* see data tab */ }:any) => (
+    <ResponsivePie
+      data={data}
+    //   margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
+    //     innerRadius={0.5}
+    //     padAngle={0.7}
+    //     cornerRadius={3}
+    //     activeOuterRadiusOffset={8}
+    //     borderWidth={1}
+    //     borderColor={{
+    //         from: 'color',
+    //         modifiers: [
+    //             [
+    //                 'darker',
+    //                 0.2
+    //             ]
+    //         ]
+    //     }}
+    //     arcLinkLabelsSkipAngle={10}
+    //     arcLinkLabelsTextColor="#333333"
+    //     arcLinkLabelsThickness={2}
+    //     arcLinkLabelsColor={{ from: 'color' }}
+    //     arcLabelsSkipAngle={10}
+    //     arcLabelsTextColor={{
+    //         from: 'color',
+    //         modifiers: [
+    //             [
+    //                 'darker',
+    //                 2
+    //             ]
+    //         ]
+    //     }}
+
+    />
+  );
+  
