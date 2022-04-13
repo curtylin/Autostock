bcbcfd1217c6c7428036e883c77a55ba84fe0dec
rftf <u1025269@utah.edu>
2022-04-12 22:10:59 -0600

    Fixed Recommendation

--- AutoStock-React/src/components/data_components/Recommendations.tsx
+++ AutoStock-React/src/components/data_components/Recommendations.tsx
@@ -1,17 +1,35 @@
+// @ts-nocheck
 import { Table, TableCell, TableContainer, TableHead, TableRow, TableBody, Paper} from '@mui/material'
 import React from 'react'
 import { useEffect } from 'react'
+import { Radar } from 'react-chartjs-2';
+import {
+  Chart as ChartJS,
+  RadialLinearScale,
+  PointElement,
+  LineElement,
+  Filler,
+  Tooltip,
+  Legend,
+} from 'chart.js';
 
-
+ChartJS.register(
+  RadialLinearScale,
+  PointElement,
+  LineElement,
+  Filler,
+  Tooltip,
+  Legend
+);
 
 export default function Recommendations({stock}: any) {
 
-  const [stockRecommendations, setStockRecommendations] = React.useState([])
+  const [stockRecommendations, setStockRecommendations] = React.useState(null)
 
 
   useEffect(() => {
     if(stock !== '') {
-      fetch(`http://localhost:5000/getSustainability/${stock}`)
+      fetch(`http://localhost:5000/getRecommendations/${stock}`)
         .then(res => res.json())
         .then(result => {
           setStockRecommendations(result)
@@ -21,27 +39,8 @@ export default function Recommendations({stock}: any) {
 
   return (
     <div>
-      <TableContainer component = {Paper}>
-        <Table aria-label="simple table">
-
-          <TableHead>
-            <TableRow>
-              <TableCell>Institution</TableCell>
-              <TableCell>Recommendation</TableCell>
-            </TableRow>
-          </TableHead>
-
-          <TableBody>
-            {stockRecommendations.map((sustainability: any) => (
-              <TableRow key={sustainability.id} sx={{ "&:last-child td, &:last-child th": { border: 0 } }}>
-                <TableCell>{sustainability.name}</TableCell>
-                <TableCell>{sustainability.value}</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-
-        </Table>
-      </TableContainer>
+      <h5>Recommendations</h5>
+      {stockRecommendations !== null  ? <Radar data={stockRecommendations}/> : null}
     </div>
   )
 }
